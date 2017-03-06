16a675133cb43fd2fbb731db07fd6f1249469366
Le Deng <z00382545@z00382545-mac.local>
2017-03-06 16:37:44 -0500

    lateset

--- store-front/src/app/app.module.ts
+++ store-front/src/app/app.module.ts
@@ -3,20 +3,32 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material';
+import { routing }  from './app.routing';
+
+import 'hammerjs';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+
+import { LoginService } from './services/login.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
+    routing,
     MaterialModule
   ],
-  providers: [],
+  providers: [
+    LoginService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ store-front/src/app/app.routing.ts
@@ -0,0 +1,22 @@
+/**
+ * Created by z00382545 on 1/16/17.
+ */
+import {ModuleWithProviders}  from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+
+import {HomeComponent} from './components/home/home.component';
+
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  }
+];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
